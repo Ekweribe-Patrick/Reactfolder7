1147a59fbdbd7670f9ce63ec3e2c3d2bea67fb86
Ekweribe-Patrick <ekweribepatrick347@gmail.com>
2025-12-11 13:01:57 +0100

    added a few corrections

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='h-screen flex justify-center items-center'>About Page</div>
+    <div className='h-screen flex justify-center items-center'>About Page 2</div>
   )
 }
 
